79d85ea2f0f3b766426f0a572e2f09aa17d7c1e8
David Forer <david.j.forer@gmail.com>
2019-08-30 08:42:59 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,13 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name => params[:landmark][:name],
+      :year_completed => params[:landmark][:year_completed])
+      
+      redirect to '/landmarks'
+    end
 end
