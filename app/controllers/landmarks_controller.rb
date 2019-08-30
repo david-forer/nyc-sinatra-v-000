1fd716ff3e069be73deada55db6b77b24f2d4d5b
David Forer <david.j.forer@gmail.com>
2019-08-30 12:50:24 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,4 +10,28 @@ class LandmarksController < ApplicationController
       
       redirect to '/landmarks'
     end
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+   @landmark = Landmark.find(params[:id])
+   erb :'/landmarks/edit'
+ end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(:name => params[:landmark][:name],
+      :year_completed => params[:landmark][:year_completed])
+    
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
+
